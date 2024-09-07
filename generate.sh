9870a95e4107a82d213ae3af9d8d06af1342ddd3
viktorbasta <viktor.blondal@basta.ai>
2024-09-07 22:18:23 +0000

    .

--- generate.sh
+++ generate.sh
@@ -1,11 +1,11 @@
 typescript () {
   echo "Generating SDK with TypeScript and Axios"
 
-  dir="clients/typescript"
+  dir="clients/typescript-axios/_generated"
   rm -rf "$dir" || true
   mkdir -p "$dir"
 
-  openapi-generator-cli generate -i ../swagger.yaml -g typescript-axios -o ./sdk
+  openapi-generator-cli generate -i swagger.yaml -g typescript-axios -o ./$dir
 }
 
 typescript
